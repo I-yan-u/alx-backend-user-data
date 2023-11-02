473c47cdc2259380b6ea76ed002738451ded8ecc
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-11-02 22:08:21 +0100

    filtered logger with redaction

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -10,5 +10,5 @@ def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     """filter datum
     """
-    pattern = '|'.join(map(re.escape, fields))
-    return re.sub(f'({pattern})=[^({separator})]+', f'\\1={redaction}', message)
+    pat = '|'.join(map(re.escape, fields))
+    return re.sub(f'({pat})=[^({separator})]+', f'\\1={redaction}', message)
