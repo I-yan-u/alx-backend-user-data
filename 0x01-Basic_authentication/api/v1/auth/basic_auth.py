6de9144562f354927429d52a5635a9fa8e89598a
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-11-07 22:31:10 +0100

    Basic auth class

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -10,4 +10,19 @@ class BasicAuth(Auth):
     Args:
         Auth (_type_): Authentication class to inherit.
     """
-    pass
+    def extract_base64_authorization_header(self, 
+                                            authorization_header: str) -> str:
+        """Extract utf-8 format of the authorization header
+
+        Args:
+            authorization_header (str): base64 encoded auth header string
+
+        Returns:
+            str: utf-8 format of the authorization header
+        """
+        ah = authorization_header
+        if ah is None or isinstance(ah, str) == False:
+            return None
+        if not ah.startswith('Basic '):
+            return None
+        return ah[6:]
