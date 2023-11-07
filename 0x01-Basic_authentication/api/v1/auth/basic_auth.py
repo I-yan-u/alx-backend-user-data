e7b64b8237dadede83ab1840f02527609bf277fa
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-11-07 22:15:26 +0100

    Basic Auth class

---
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+""" Basic_auth implementation
+"""
+from api.v1.auth.auth import Auth
+
+
+class BasicAuth(Auth):
+    """To be consructed
+
+    Args:
+        Auth (_type_): Authentication class to inherit.
+    """
+    pass
