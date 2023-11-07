d299b834fa465ecff679ade1582cf25722c5bd85
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-11-07 23:02:27 +0100

    tips

--- 0x01-Basic_authentication/main.py
+++ 0x01-Basic_authentication/main.py
@@ -1,10 +1,14 @@
 #!/usr/bin/env python3
-""" Main 0
+""" Main 3
 """
-from api.v1.auth.auth import Auth
+from api.v1.auth.basic_auth import BasicAuth
 
-a = Auth()
+a = BasicAuth()
 
-print(a.require_auth("/api/v1/status/", ["/api/v1/status/"]))
-print(a.authorization_header())
-print(a.current_user())
+print(a.decode_base64_authorization_header(None))
+print(a.decode_base64_authorization_header(89))
+print(a.decode_base64_authorization_header("Holberton School"))
+print(a.decode_base64_authorization_header("SG9sYmVydG9u"))
+print(a.decode_base64_authorization_header("SG9sYmVydG9uIFNjaG9vbA=="))
+print(a.decode_base64_authorization_header(a.extract_base64_authorization_header("Basic SG9sYmVydG9uIFNjaG9vbA==")))
+
