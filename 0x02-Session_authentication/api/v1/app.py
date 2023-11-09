ff667385321191a40db407fb983792832616ed55
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-11-09 23:14:34 +0100

    Session Auth class

--- 0x02-Session_authentication/api/v1/app.py
+++ 0x02-Session_authentication/api/v1/app.py
@@ -54,10 +54,12 @@ def check_auth():
     excluded_list = [
         '/api/v1/status/',
         '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
+        '/api/v1/forbidden/',
+        '/api/v1/auth_session/login/'
         ]
     if auth.require_auth(request.path, excluded_list) is True:
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None\
+          and auth.session_cookies(request) is None:
             abort(401)
         if auth.current_user(request) is None:
             abort(403)
