507d7efa0628107acc16cfcf317317bab24b39cd
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-11-12 01:08:44 +0100

    Session Auth class

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 """
 from api.v1.auth.auth import Auth
 import uuid
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -41,3 +42,18 @@ class SessionAuth(Auth):
             return None
         user_id = self.user_id_by_session_id.get(session_id, None)
         return user_id
+
+    def current_user(self, request=None):
+        """Current user
+
+        Args:
+            request (_type_, optional): Request header. Defaults to None.
+        """
+        session_id = self.session_cookie(request)
+
+        if session_id is None:
+            return None
+
+        user_id = self.user_id_for_session_id(session_id)
+
+        return User.get(user_id)
