4d3cfd914a8e0ece8396d79cea95120ee1efea06
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-11-15 22:29:29 +0100

    User Auth service

--- 0x03-user_authentication_service/user.py
+++ 0x03-user_authentication_service/user.py
@@ -3,11 +3,19 @@
 """
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, String
+
+
 Base = declarative_base()
 
 
 class User(Base):
+    """User model for auth service
+
+    Args:
+        Base (sqlalchemy): sqlalchemy declarative base class
+    """
     __tablename__ = 'users'
+
     id = Column(Integer, primary_key=True)
     email = Column(String(250), nullable=False)
     hashed_password = Column(String(250), nullable=False)
