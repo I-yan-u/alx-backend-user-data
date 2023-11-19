c4414c690c26521fe937aff84a5fd9b0674d12e6
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-11-19 14:46:01 +0100

    User Auth service

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -111,3 +111,19 @@ class Auth:
             user.session_id = None
         except NoResultFound:
             raise NoResultFound
+
+    def get_reset_password_token(self, email: str) -> str:
+        """Get the reset password token from the database
+
+        Args:
+            email (str): user email address.
+
+        Returns:
+            str: user reset password token.
+        """
+        try:
+            user = self._db.find_user_by(email=email)
+            user.reset_token = _generate_uuid()
+            return user.reset_token
+        except NoResultFound:
+            raise ValueError
