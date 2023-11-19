30444c27c2047a94e7cb46684e7b52c2f80743a4
Iyanu Ajimobi <iyanuajimbi12@gmail.com>
2023-11-19 08:59:46 +0100

    User Auth service

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python3
 """Basic Flasl app
 """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from auth import Auth
+# from typing import Union
 
 
+AUTH = Auth()
 app = Flask(__name__)
 
 
@@ -12,6 +15,20 @@ def index():
     """index route
     """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route('/users', methods=['POST'])
+def users():
+    """register user routes
+    """
+    email = request.form.get('email')
+    password = request.form.get('password')
+    try:
+        user = AUTH.register_user(email, password)
+        return jsonify({"email": f"{user.email}",
+                        "message": "user created"})
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
 
 
 if __name__ == '__main__':
